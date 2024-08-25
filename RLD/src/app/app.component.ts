557b010c615a835ecb01a7526b755a54b0bb3f63
ErnestGeo24 <ernest1.avasi@gmail.com>
2024-08-25 09:57:45 +0000

    8.1

--- RLD/src/app/app.component.ts
+++ RLD/src/app/app.component.ts
@@ -1,13 +1,18 @@
-import { Component } from '@angular/core';
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
 
-@Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+import { AppComponent } from './app.component';
+import { ArticleComponent } from './article/article.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    ArticleComponent
+  ],
+  imports: [
+    BrowserModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
 })
-export class AppComponent {
-  addArticle(title: HTMLInputElement, link: HTMLInputElement): boolean {
-    console.log(`Adding article title: ${title.value} and link: ${link.value}`);
-  return false;
-  }
-}
+export class AppModule { }
--- RLD/src/app/article/article.component.ts
+++ RLD/src/app/article/article.component.ts
@@ -1,29 +1,30 @@
-import { Component, OnInit, HostBinding } from '@angular/core';
+import { Component, Input, OnInit, HostBinding } from '@angular/core';
+import { Article } from './article.model'; //Non cancellare gli altri import
 
 @Component({
   selector: 'app-article',
   templateUrl: './article.component.html',
   styleUrls: ['./article.component.css']
 })
-export class ArticleComponent {
-  @HostBinding('attr.class') cssClass = 'card';
-  votes: number;
-  title: string;
-  link: string;
+export class ArticleComponent implements OnInit {
+  @Input() article: Article;
+  @HostBinding('attr.class') cssClass = 'card mt-4';
 
   constructor() {
-    this.title = 'Angular 2';
-    this.link = 'http://angular.io';
-    this.votes = 10;
+    this.article = new Article('Angular 2','http://angular.io',10);
   }
 
-  voteUp() {
-    this.votes += 1;
+  voteUp(): Boolean {
+    this.article.voteUp(); //Modificato qui 
+    return false;
   }
 
-  voteDown() {
-    this.votes -= 1;
+  voteDown():Boolean {
+    this.article.voteDown(); //Modificato qui
+    return false;
   }
 
   ngOnInit() {}
+
+  
 }
